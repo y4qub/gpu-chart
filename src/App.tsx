4d6ad28f9f3fc4d4ebe1f58123fb32a3d11bfcb7
Jakub <murcja812@gmail.com>
2022-02-25 10:29:14 +0100

    add chartjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+import faker from '@faker-js/faker'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export const options = {
+  responsive: true,
+  // plugins: {
+  // legend: {
+  //   position: 'top' as const,
+  // },
+  // title: {
+  //   display: true,
+  //   text: 'Chart.js Bar Chart',
+  // },
+  // },
+}
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+
+export const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: 'rgba(255, 99, 132, 0.9)',
+      fill: '#ffffff',
+      tension: 0.1,
+    },
+    // {
+    //   label: 'Dataset 2',
+    //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    // },
+  ],
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Line options={options} data={data} />;
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
